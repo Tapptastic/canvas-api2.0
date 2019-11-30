b9a54039d3f3a00dbfa61efc8e9cbcd63189ea09
Grant <grant@Grants-MacBook-Pro.local>
2019-11-30 19:13:53 +0000

    Added rotation for triangles

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 namespace Engine {
 
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
+    export const canvas = document.createElement('canvas');
+    export const context = canvas.getContext('2d');
 
     export function Start() {
         document.body.append(canvas);
@@ -34,10 +34,19 @@ namespace Engine {
     }
 
     export class Triangle {
-        Draw(point: Point, point1: Point, point2: Point) {
-            Line.Draw(point, point1);
-            Line.Draw(point1, point2);
-            Line.Draw(point2, point);
+        Points: Point[];
+
+        constructor(point: Point, point1: Point, point2: Point) {
+            this.Points = [];
+            this.Points.push(point);
+            this.Points.push(point1);
+            this.Points.push(point2);
+        }
+
+        Draw() {
+            Line.Draw(this.Points[0], this.Points[1]);
+            Line.Draw(this.Points[1], this.Points[2]);
+            Line.Draw(this.Points[2], this.Points[0]);
         }
     }
 
@@ -81,3 +90,62 @@ namespace Engine {
 
 Engine.Start();
 
+const center = {
+    x: Engine.canvas.clientWidth / 2,
+    y: Engine.canvas.clientHeight / 2
+}
+
+function RotatePoint(point, angle) {
+    const rad = DegToRad(angle);
+    const newX = (point.x * Math.cos(rad)) - (point.y * Math.sin(rad)); //?
+    const newY = (point.x * Math.sin(rad) + (point.y * Math.cos(rad))) //?
+
+    return new Engine.Point(newX, newY);
+}
+
+function Rotate(triangle: Engine.Triangle, angle: number) {
+    const rad = DegToRad(angle);
+
+    let newPoints: Engine.Point[] = [];
+
+    triangle.Points.map(point => {
+        //Take the centre x & y from point x & y
+        //Rotate point 
+        //Add centre x & y to point
+
+        //See about world space and object space
+        newPoints.push(RotatePoint({ x: point.x , y: point.y }, angle));
+        // const newPoint = RotatePoint(new Engine.Point(
+        //     center.x - point.x,
+        //     center.y - point.y
+        // ), angle)
+
+        // newPoints.push(
+        //     new Engine.Point(newPoint.x + center.x, newPoint.y + center.y)
+        // );
+    });
+
+    return newPoints;
+}
+
+function DegToRad(deg) {
+    return deg * Math.PI / 180;
+}
+
+// Engine.context.translate(Engine.canvas.clientWidth / 2, Engine.canvas.clientHeight / 2);
+
+const p = new Engine.Point(20, 20);
+const p1 = new Engine.Point(40, 40);
+const p2 = new Engine.Point(60, 20);
+
+const t = new Engine.Triangle(p, p1, p2);
+
+const points = Rotate(t, 13);
+const t1 = new Engine.Triangle(
+    points[0],
+    points[1],
+    points[2]
+);
+
+t.Draw();
+t1.Draw();
